9656ac958fa95b089f0a1fe50d48e6368b21d67b
osamurakouhei <k.osamura001012@ezweb.ne.jp>
2024-01-24 21:28:36 +0900

    テストコード作成

--- receive_jira_webhook/main.go
+++ receive_jira_webhook/main.go
@@ -1,46 +1,44 @@
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-lambda-go/lambda"
+	"context"
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 type JiraWebhookRequest struct {
-    Issue Issue `json:"issue"`
+	Issue Issue `json:"issue"`
 }
 
 type Issue struct {
-    Fields Fields `json:"fields"`
+	Fields Fields `json:"fields"`
 }
 
 type Fields struct {
-    Description string `json:"description"`
+	Description string `json:"description"`
 }
 
-func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    var jiraReq JiraWebhookRequest
-    err := json.Unmarshal([]byte(request.Body), &jiraReq)
-    if err != nil {
-        return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
-    }
-
-    description := jiraReq.Issue.Fields.Description
-
-    responseBody := map[string]string{
-        "description": description,
-    }
-
-    jsonBody, err := json.Marshal(responseBody)
-    if err != nil {
-        return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
-    }
-
-    return events.APIGatewayProxyResponse{Body: string(jsonBody), StatusCode: 200}, nil
+type Response struct {
+	Body       string `json:"body"`
+	StatusCode int    `json:"statuscode"`
 }
 
+func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (Response, error) {
+	var jiraReq JiraWebhookRequest
+	err := json.Unmarshal([]byte(request.Body), &jiraReq)
+	if err != nil {
+		return Response{Body: "Unable to parse JSON from request", StatusCode: 400}, err
+	}
+
+	description := jiraReq.Issue.Fields.Description
+	if description == "" {
+		return Response{Body: "Empty Description in JSON", StatusCode: 400}, nil
+	}
+
+	return Response{Body: description, StatusCode: 200}, nil
+}
 
 func main() {
-    lambda.Start(HandleRequest)
+	lambda.Start(HandleRequest)
 }
---
+++ receive_jira_webhook/main_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"testing"
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func TestHandleRequest(t *testing.T) {
+	type args struct {
+		ctx     context.Context
+		request events.APIGatewayProxyRequest
+	}
+	tests := []struct {
+		name            string
+		args            args
+		wantStatusCode  int
+		wantErr         bool
+		wantResponseBody string
+	}{
+		{
+			name: "Valid Request",
+			args: args{
+				ctx: context.Background(),
+				request: events.APIGatewayProxyRequest{
+					Body: `{"issue": {"fields": {"description": "Test description"}}}`,
+				},
+			},
+			wantStatusCode:  200,
+			wantErr:         false,
+			wantResponseBody: "Test description",
+		},
+        {
+            name: "Invalid JSON",
+			args: args{
+				ctx: context.Background(),
+				request: events.APIGatewayProxyRequest{
+					Body: `{invalid_json}`,
+				},
+			},
+			wantStatusCode: 400,
+			wantErr: true,
+			wantResponseBody: "Unable to parse JSON from request",
+        },
+		{
+			name: "Empty Description in JSON",
+			args: args{
+				ctx: context.Background(),
+				request: events.APIGatewayProxyRequest{
+					Body: `{"issue": {"fields": { "description": "" }}}`,
+				},
+			},
+			wantStatusCode: 400,
+			wantErr: false,
+			wantResponseBody: "Empty Description in JSON",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := HandleRequest(tt.args.ctx, tt.args.request)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("HandleRequest() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got.StatusCode != tt.wantStatusCode {
+				t.Errorf("HandleRequest() StatusCode = %v, want %v", got.StatusCode, tt.wantStatusCode)
+			}
+			if got.Body != tt.wantResponseBody {
+				t.Errorf("HandleRequest() Body = %v, want %v", got.Body, tt.wantResponseBody)
+			}
+		})
+	}
+}
