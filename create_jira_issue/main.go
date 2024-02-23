fbb75855d802cedc33f33bbf214116e135ffb51d
osamurakouhei <k.osamura001012@ezweb.ne.jp>
2024-02-23 14:49:32 +0900

    refactoring

--- create_jira_issue/main.go
+++ create_jira_issue/main.go
@@ -16,15 +16,43 @@ type Request struct {
 	StatusCode int    `json:"statuscode"`
 }
 
+type JiraIssue struct {
+	Fields Field `json:"fields"`
+}
+
+type Field struct {
+	Project     Project   `json:"project"`
+	Summary     string    `json:"summary"`
+	Description string    `json:"description"`
+	Issuetype   Issuetype `json:"issuetype"`
+}
+
+type Project struct {
+	Key string `json:"key"`
+}
+
+type Issuetype struct {
+	Name string `json:"name"`
+}
+
 type Response struct {
 	Body       string `json:"body"`
 	StatusCode int    `json:"statuscode"`
 }
 
 func HandleRequest(ctx context.Context, request Request) (Response, error) {
-	summary := "お問い合わせ内容要約"
-	description := request.Body
-	err := CreateJiraIssue(summary, description)
+	jiraIssue := JiraIssue{
+		Fields: Field{
+			Project:     Project{Key: "KOFL"},
+			Summary:     "お問い合わせ内容の要約",
+			Description: request.Body,
+			Issuetype:   Issuetype{Name: "Task"},
+		},
+	}
+	jiraURL := os.Getenv("JIRA_URL")
+	jiraEmail := os.Getenv("JIRA_EMAIL")
+	apiToken := os.Getenv("JIRA_API_TOKEN")
+	err := CreateJiraIssue(jiraIssue, jiraURL, jiraEmail, apiToken)
 	if err != nil {
 		return Response{Body: "Unable to Create Jira Issue", StatusCode: 500}, err
 	}
@@ -32,24 +60,8 @@ func HandleRequest(ctx context.Context, request Request) (Response, error) {
 	return Response{Body: "Jira Issue Created", StatusCode: 200}, nil
 }
 
-func CreateJiraIssue(summary, description string) error {
-	jiraURL := os.Getenv("JIRA_URL")
-	apiToken := os.Getenv("JIRA_API_TOKEN")
-
-	payload := map[string]interface{}{
-		"fields": map[string]interface{}{
-			"project": map[string]string{
-				"key": "KOFL",
-			},
-			"summary":     summary,
-			"description": description,
-			"issuetype": map[string]string{
-				"name": "Task",
-			},
-		},
-	}
-
-	requestBody, err := json.Marshal(payload)
+func CreateJiraIssue(jiraIssue JiraIssue, jiraURL, jiraEmail, apiToken string) error {
+	requestBody, err := json.Marshal(jiraIssue)
 	if err != nil {
 		return fmt.Errorf("error marshalling Jira request: %w", err)
 	}
@@ -60,7 +72,7 @@ func CreateJiraIssue(summary, description string) error {
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Basic "+apiToken)
+	req.SetBasicAuth(jiraEmail, apiToken)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
